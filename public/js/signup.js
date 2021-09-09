b3b5149e773bd88dc4f7b5db8cd2ba8fd9640cba
John-Mark Sackett <jmsackett91@gmail.com>
2021-09-09 17:43:28 -0400

    finally finished my css and fixed a syntax error in my api routes file

--- public/js/signup.js
+++ public/js/signup.js
@@ -16,7 +16,6 @@ async function signupFormHandler(event) {
         if (response.ok) {
             console.log('success');
 
-
             document.location.replace('/dashboard');
 
         } else {
